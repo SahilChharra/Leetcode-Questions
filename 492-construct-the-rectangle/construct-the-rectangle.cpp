8a7ec041c1c98753daecbace29b49a661a93c27d
SahilChharra <113119610+SahilChharra@users.noreply.github.com>
2023-07-26 18:36:22 +0530

    Time: 3 ms (48.56%) | Memory: 6.2 MB (21.47%) - LeetSync

---
+++ 492-construct-the-rectangle/construct-the-rectangle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> constructRectangle(int area) {
+        int ans=0;
+        int l=1;
+        int b=1;
+        for(int i=1;i<=sqrt(area);i++){
+            if(area%i==0){
+                l=area/i;
+                b=i;
+            }
+        }
+
+        return {l,b};
+    }
+};
