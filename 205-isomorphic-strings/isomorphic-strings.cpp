fe1379518858baddbdff29f9adbb4fe75ce0b39a
SahilChharra <113119610+SahilChharra@users.noreply.github.com>
2023-07-27 18:09:57 +0530

    Time: 3 ms (94.69%) | Memory: 6.9 MB (57.19%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char,char> rep;
+        unordered_map<char,char> used;
+
+        for(int i=0;i<s.length();i++){
+            if(rep.count(s[i])){
+                if(rep[s[i]] != t[i]){
+                    return false;
+                }
+            }
+            else{
+                if(used[t[i]]){
+                    return false;
+                }
+
+                rep[s[i]] = t[i];
+                used[t[i]] = true;
+            }
+        }
+
+        return true;
+    }
+};
