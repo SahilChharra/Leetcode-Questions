ab1b667bbf508be89d23075e8461a3e8d51af25a
SahilChharra <113119610+SahilChharra@users.noreply.github.com>
2023-08-12 22:44:41 +0530

    Time: 103 ms (93.71%) | Memory: 91.8 MB (25.36%) - LeetSync

---
+++ 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findNonMinOrMax(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        if(n<=2){
+            return -1;
+        }
+        else{
+            return nums[1];
+        }
+    }
+};
