08124feed9d10c652e260b80a683fd32978e071e
Sahil Chharra <113119610+SahilChharra@users.noreply.github.com>
2024-05-18 20:34:15 +0530

    Time: 3 ms (80.19%) | Memory: 15.9 MB (6.74%) - LeetSync

---
+++ 1021-distribute-coins-in-binary-tree/distribute-coins-in-binary-tree.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    using node2 = tuple<TreeNode*, TreeNode*, bool>; // Node, parent & visited flag
+    static int distributeCoins(TreeNode* root) {
+        if (!root) return 0;
+        vector<node2> stack = {{root, NULL, 0}};
+        int moves=0;
+        while (!stack.empty()) {
+            auto [node, parent, visited] = stack.back();
+            stack.pop_back();
+
+            if (!visited) {
+                // Mark the node as visited & repush
+                stack.emplace_back(node, parent, 1);
+
+                // Push right and left children onto the stack
+                if (node->right) 
+                    stack.emplace_back(node->right, node, 0);
+                if (node->left) 
+                    stack.emplace_back(node->left, node, 0);
+            } 
+            else {
+                int x=node->val-1;// give x coins to parent node
+                if (parent) parent->val += x; // now parent's coins
+                moves+=abs(x);
+            }
+        }
+
+        return moves;
+    }
+};
